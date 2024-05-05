56365ec104b1b37774df1f2b2da657bd6b52220d
Lance Hart <me@lancehart.ca>
2024-05-05 00:36:55 -0400

    Updated templates

--- flake.nix
+++ flake.nix
@@ -23,25 +23,27 @@
       ];
     };
 
-    templates.system = {
-      path = ./templates/system;
-      description = "A minimal system configuration";
-      welcomeText = ''
-        # Minimal lsuite system configuration
-        ## To do before running
-         1. Set the `hostname` in "flake.nix"
-         2. Set the `description` in "flake.nix"
-         3. Set the `user.password` in "configuration.nix" to the hashed password
-         4. Add apps to `apps.list` in "configuration.nix"
-        
-        ## Usage
-        Once the configuration is setup, the state can be set with:
-        ```sh
-        sudo nixos-rebuild switch --flake .
-        ```
-      '';
-    };
+    templates = {
+      default = self.templates.system;
 
-    templates.default = self.templates.system;
+      system = {
+        path = ./templates/system;
+        description = "A minimal system configuration";
+        welcomeText = ''
+          # Minimal lsuite system configuration
+          ## To do before running
+          1. Set the `hostname` in "flake.nix"
+          2. Set the `description` in "flake.nix"
+          3. Set the `user.password` in "configuration.nix" to the hashed password
+          4. Add apps to `apps.list` in "configuration.nix"
+          
+          ## Usage
+          Once the configuration is setup, the state can be set with:
+          ```sh
+          sudo nixos-rebuild switch --flake .
+          ```
+        '';
+      };
+    };
   };
 }
